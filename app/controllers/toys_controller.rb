b24aa794bf691ce692ed0f55883d9d1f6b15c155
catherineotondi <catherineotondi@gmail.com>
2022-12-05 20:38:40 +0300

    completed

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -5,17 +5,24 @@ class ToysController < ApplicationController
     toys = Toy.all
     render json: toys
   end
-
+# Debugging tip
+# Change Toys.all to Toy.all in line 10
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
+  # DEBUGGING TIP
+  # Solved Unhandled Rejection (SyntaxError): Unexpected end of JSON input by rendering toy in JSON format
   def update
     toy = Toy.find_by(id: params[:id])
     toy.update(toy_params)
+
+    render json: toy
   end
 
+  # DEBUGGING TIP
+  # Solved ActionController::RoutingError (No route matches [DELETE] "/toys/2") by adding :destroy in the routes
   def destroy
     toy = Toy.find_by(id: params[:id])
     toy.destroy
